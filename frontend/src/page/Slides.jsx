2c81836cb5cc3dabcb66afe1244608e9a6489079
HaraprasadTripathy <haraprasad@evincedev.com>
2023-03-16 17:26:53 +0530

    added slide component

---
+++ frontend/src/page/Slides.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Slides.css'
+
+function Slides() {
+    return (
+        <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item active" data-bs-interval="3000">
+                    <img src="../img/2593170.png" className="d-block w-100" style={{ borderRadius: "2em" }} alt="..." />
+                </div>
+                <div className="carousel-item" data-bs-interval="3000">
+                    <img src="../img/2593170.png" className="d-block w-100" style={{ borderRadius: "2em" }} alt="..." />
+                </div>
+                <div className="carousel-item " data-bs-interval="3000">
+                    <img src="../img/2593170.png" className="d-block w-100" style={{ borderRadius: "2em" }} alt="..." />
+                </div>
+                <div className="carousel-item" data-bs-interval="3000">
+                    <img src="../img/2593170.png" className="d-block w-100" style={{ borderRadius: "2em" }} alt="..." />
+                </div>
+                <div className="carousel-item" data-bs-interval="3000">
+                    <img src="../img/2593170.png" className="d-block w-100" style={{ borderRadius: "2em" }} alt="..." />
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+    )
+}
+
+export default Slides;
