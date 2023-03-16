dafd66428acb6f7320489256a0abc66283b0e03a
SumitKumarPatro <sumitm@evincedev.com>
2023-03-16 14:33:24 +0530

    added searchbar to navbar and added css

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -2,15 +2,14 @@ import React, { useContext } from 'react'
 import './Navbar.css'
 import { NavLink } from 'react-router-dom'
 import { DataContext } from '../context/DataContext'
-
+import { BiSearch } from "react-icons/bi";
 const Navbar = () => {
   const { cart } = useContext(DataContext)
-  // console.log(cart)
 
   return (
     <>
-      <div className="code-nav">
-        <nav>
+      <div className="code-nav flex">
+        <nav className='right-nav flex'>
           <input type="checkbox" id="check" />
           <label htmlFor="check" className="checkbtn">
             <i className="fa fa-bars"></i>
@@ -18,7 +17,11 @@ const Navbar = () => {
           <label className="logo">
             <NavLink to="/home">Cart.Com</NavLink>
           </label>
-          <ul>
+
+          <ul className='flex'>
+            <label className='searchicon'><BiSearch /> <input placeholder="Search for products, brands and more" className='searchbar' value="" /></label>
+            <li></li>
+            <li></li>
             <li><NavLink to="/home"  >Home</NavLink></li>
             <li><NavLink to="/Products">Products</NavLink></li>
 
@@ -26,6 +29,7 @@ const Navbar = () => {
             <li><NavLink to="/cart" className="cart-box">Cart <span>{cart.length}</span> </NavLink></li>
             <li><NavLink to="/myaccount" >User </NavLink></li>
           </ul>
+
         </nav>
       </div>
 
