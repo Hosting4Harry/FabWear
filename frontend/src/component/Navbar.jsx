12285740b735cac3e830e65be468e1a1eb8e8c05
HaraprasadTripathy <haraprasad@evincedev.com>
2023-03-16 15:53:18 +0530

    did changes in searchbar

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import './Navbar.css'
 import { NavLink } from 'react-router-dom'
 import { DataContext } from '../context/DataContext'
-import { BiSearch } from "react-icons/bi";
+import { FcSearch } from "react-icons/fc";
 const Navbar = () => {
   const { cart } = useContext(DataContext)
 
@@ -17,14 +17,13 @@ const Navbar = () => {
           <label className="logo">
             <img style={{ width: "50px" }} src="../img/T4.png" alt="box" className="img-fluid" /><NavLink to="/home">Cart.Com</NavLink>
           </label>
-
           <ul className='flex'>
-            <label className='searchicon'><BiSearch /> <input placeholder="Search for products, brands and more" className='searchbar' value="" /></label>
-            <li></li>
-            <li></li>
+            <label className='searchicon'>
+              <input placeholder="Search for products, brands and more" className='searchbar ' value="" />
+              <button className='btn-sm border border-dark ' > <FcSearch /></button>
+            </label>
             <li><NavLink to="/home"  >Home</NavLink></li>
             <li><NavLink to="/Products">Products</NavLink></li>
-
             <li><NavLink to="/contact">Contact</NavLink></li>
             <li><NavLink to="/cart" className="cart-box">Cart <span>{cart.length}</span> </NavLink></li>
             <li><NavLink to="/myaccount" >User </NavLink></li>
