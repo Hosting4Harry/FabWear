9d604b60849bc2eebbd9cb78eb5fd98d95ab4701
HaraprasadTripathy <haraprasad@evincedev.com>
2023-03-16 18:40:03 +0530

    edited some images from slide

--- frontend/src/page/ProductDetails.jsx
+++ frontend/src/page/ProductDetails.jsx
@@ -34,7 +34,7 @@ const ProductDetails = () => {
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
-    console.log(pdetails)
+
     const onSub = (e) => {
         e.preventDefault();
         const data = {
--- frontend/src/page/Slides.jsx
+++ frontend/src/page/Slides.jsx
@@ -9,16 +9,16 @@ function Slides() {
                     <img src="../img/2593170.png" className="d-block w-100" style={{ borderRadius: "2em" }} alt="..." />
                 </div>
                 <div className="carousel-item" data-bs-interval="3000">
-                    <img src="../img/2593170.png" className="d-block w-100" style={{ borderRadius: "2em" }} alt="..." />
+                    <img src="../img/E1.jpg" className="d-block w-100" style={{ borderRadius: "2em" }} alt="..." />
                 </div>
                 <div className="carousel-item " data-bs-interval="3000">
-                    <img src="../img/2593170.png" className="d-block w-100" style={{ borderRadius: "2em" }} alt="..." />
+                    <img src="../img/E2.jpg" className="d-block w-100" style={{ borderRadius: "2em" }} alt="..." />
                 </div>
                 <div className="carousel-item" data-bs-interval="3000">
-                    <img src="../img/2593170.png" className="d-block w-100" style={{ borderRadius: "2em" }} alt="..." />
+                    <img src="../img/E3.jpg" className="d-block w-100" style={{ borderRadius: "2em" }} alt="..." />
                 </div>
                 <div className="carousel-item" data-bs-interval="3000">
-                    <img src="../img/2593170.png" className="d-block w-100" style={{ borderRadius: "2em" }} alt="..." />
+                    <img src="../img/E4.jpg" className="d-block w-100" style={{ borderRadius: "2em" }} alt="..." />
                 </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
