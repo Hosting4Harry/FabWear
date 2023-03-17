83ea5551f2006a0b411817b1b3a90d6a04013e6b
HaraprasadTripathy <haraprasad@evincedev.com>
2023-03-17 15:45:47 +0530

    did changes will update lated

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -6,12 +6,30 @@ import { DataContext } from '../context/DataContext'
 const Navbar = () => {
   const { cart } = useContext(DataContext)
   const [inputValue, setInputValue] = useState('');
+  const search_animal = () => {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('animals');
+    console.log("first")
+    for (let i = 0; i < x.length; i++) {
+      if (inputValue === "") {
+        x[i].style.display = "none";
+      }
+      else if (!x[i].innerHTML.toLowerCase().includes(input)) {
+        x[i].style.display = "none";
+      }
+      else {
+        x[i].style.display = "list-item";
+      }
+    }
+  }
+
   const submit = (e) => {
     e.preventDefault();
     setInputValue('');
   };
-  useEffect(() => {
-  }, [inputValue])
+  // useEffect(() => {
+  // }, [inputValue])
   return (
     <>
       <div className="code-nav flex">
@@ -27,8 +45,29 @@ const Navbar = () => {
             <li>
               <form onSubmit={submit}>
                 <div className='d-flex form-group '>
-                  <input size={30} placeholder="Search for products, brands and more" className='form-control form-group-sm' defaultValue={inputValue} onChange={(e) => { setInputValue(e.target.value) }} />
-                  <button type='submit' className='btn btn-primary' style={{ padding: "5px 15px", borderRadius: "3px" }}> <i className="fa fa-search"></i></button>
+                  <div className=''>
+                    <div className=''>
+                      <input type='text' size={30} onKeyUp={search_animal} id='searchbar' placeholder="Search for products, brands and more" className='form-control form-group-sm' defaultValue={inputValue} onChange={(e) => { setInputValue(e.target.value) }} />
+                    </div>
+                    <div className='bg-dark'>
+                      <ol id='list overlay'>
+                        <li className="animals" style={{ display: 'none' }}>Cat</li>
+                        <li className="animals" style={{ display: 'none' }}>Dog</li>
+                        <li className="animals" style={{ display: 'none' }}>Elephant</li>
+                        <li className="animals" style={{ display: 'none' }}>Fish</li>
+                        <li className="animals" style={{ display: 'none' }}>Gorilla</li>
+                        <li className="animals" style={{ display: 'none' }}>Monkey</li>
+                        <li className="animals" style={{ display: 'none' }}>Turtle</li>
+                        <li className="animals" style={{ display: 'none' }}>Whale</li>
+                        <li className="animals" style={{ display: 'none' }}>Aligator</li>
+                        <li className="animals" style={{ display: 'none' }}>Donkey</li>
+                        <li className="animals" style={{ display: 'none' }}>Horse</li>
+                      </ol>
+                    </div>
+                  </div>
+                  <div className=''>
+                    <button type='submit' className='btn btn-primary m-0' style={{ padding: "5px 15px", borderRadius: "3px" }}> <i className="fa fa-search"></i></button>
+                  </div>
                 </div>
               </form>
             </li>
