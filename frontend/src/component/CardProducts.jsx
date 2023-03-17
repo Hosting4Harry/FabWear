e1bbf2da7e75cd716565b3ef7896a89ae9b47605
SumitKumarPatro <sumitm@evincedev.com>
2023-03-17 13:04:34 +0530

    heart button added to cart

--- frontend/src/component/CardProducts.jsx
+++ frontend/src/component/CardProducts.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-
+import "../App.css";
+// import { GrFavorite } from 'react-icons/gr';
 const CardProducts = ({ id, name, price, product_image }) => {
     const navigate = useNavigate();
+    const handelChange = (e) => {
 
+        alert("hello")
+    }
     return (
         <>
             <div className="col-lg-4 col-md-6 col-12  mb-3 products-p">
                 <div className="card p-2">
+                    <div>
+                        <input type="checkbox" id={"heart" + id} />
+                        <label for={"heart" + id}>&#9829;</label>
+                    </div>
                     <img src={`../img/${product_image}`} alt="tree" className="img-fluid p-img" />
                     <div className="overlay">
                         <div className="price">
@@ -21,7 +29,6 @@ const CardProducts = ({ id, name, price, product_image }) => {
                     </div>
                 </div>
             </div>
-
         </>
     )
 }
