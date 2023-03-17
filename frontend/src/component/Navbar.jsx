2987d0bc7491e719679bd1dc54ebfe14a3dcda32
HaraprasadTripathy <haraprasad@evincedev.com>
2023-03-17 11:31:11 +0530

    did changes in search bar

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { NavLink } from 'react-router-dom'
 import { DataContext } from '../context/DataContext'
-import { FcSearch } from "react-icons/fc";
+
 const Navbar = () => {
   const { cart } = useContext(DataContext)
   const [inputValue, setInputValue] = useState('');
@@ -18,10 +18,14 @@ const Navbar = () => {
             <img style={{ width: "50px" }} src="../img/T4.png" alt="box" className="img-fluid" /><NavLink to="/home">Cart.Com</NavLink>
           </label>
           <ul className='flex'>
-            <label className='searchicon'>
-              <input placeholder="Search for products, brands and more" className='searchbar ' defaultValue={inputValue} onChange={(e) => { setInputValue(e.target.value) }} />
-              <button className='btn-sm border border-dark ' > <FcSearch /></button>
-            </label>
+            <li>
+              <form onSubmit=''>
+                <div className='d-flex form-group '>
+                  <input size={30} placeholder="Search for products, brands and more" className='form-control form-group-sm' defaultValue={inputValue} onChange={(e) => { setInputValue(e.target.value) }} />
+                  <button type='submit' className='btn btn-primary' style={{ padding: "5px 15px", borderRadius: "3px" }}> <i class="fa fa-search"></i></button>
+                </div>
+              </form>
+            </li>
             <li><NavLink to="/home"  >Home</NavLink></li>
             <li><NavLink to="/Products">Products</NavLink></li>
             <li><NavLink to="/contact">Contact</NavLink></li>
