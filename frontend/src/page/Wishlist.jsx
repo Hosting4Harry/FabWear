1ad7b116cc912d8987da0d0ad9b4a2c9721f68a2
SumitKumarPatro <sumitm@evincedev.com>
2023-03-17 16:16:21 +0530

    wishlist created

--- frontend/src/page/Wishlist.jsx
+++ frontend/src/page/Wishlist.jsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import React, { useContext, useRef, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+import WishlistP from '../component/WishlistP';
 import { DataContext } from '../context/DataContext'
 
 const Wishlist = () => {
-    const { Wishlist } = useContext(DataContext);
+    const { wishlist } = useContext(DataContext);
     const timeout = useRef(null);
     const navigate = useNavigate();
     const checkAuth = () => {
@@ -29,10 +30,44 @@ const Wishlist = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
-
     return (
         <>
             <h1>hello this is your wishlist</h1>
+            <div className="cart">
+                {
+                    !wishlist.length ? (
+                        <>
+                            <div className="container">
+                                <h2>There is No Items In the wishlist</h2>
+                                <button className="btn btn-info" onClick={() => navigate('/products')}>Continue Shopping</button>
+                            </div>
+
+                        </>
+                    ) : (
+                        <>
+                            <div className="container">
+                                <h2>Your wishlist Items</h2>
+                                <br />
+                                <div className="row">
+                                    {
+                                        wishlist.map((val, ind) => {
+                                            return (<WishlistP
+                                                key={ind}
+                                                id={val.id}
+                                                name={val.name}
+                                                price={val.price}
+                                                plant_image={val.image}
+                                            />
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        </>
+                    )
+                }
+            </div>
+
         </>
     )
 }
