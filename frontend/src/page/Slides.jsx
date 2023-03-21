d5441707860070ab4d50d4f1218ac50b82d35ace
HaraprasadTripathy <haraprasad@evincedev.com>
2023-03-21 17:48:19 +0530

    added wrap = true in slides

--- frontend/src/page/Slides.jsx
+++ frontend/src/page/Slides.jsx
@@ -3,12 +3,13 @@ import './Slides.css'
 
 function Slides() {
     return (
-        <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+        <div id="carouselExampleInterval" className="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-wrap='true' >
             <div className="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
             </div>
             <div className="carousel-inner">
                 <div className="carousel-item active" data-bs-interval="3000">
