d1d1d44a00c0f88d80ccd543925c3e4ded77c968
HaraprasadTripathy <haraprasad@evincedev.com>
2023-03-21 14:19:30 +0530

    did changes in navbar and changed the class to className

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -13,36 +13,33 @@ const Navbar = () => {
   };
   // useEffect(() => {
   // }, [inputValue])
-  return (
-    <>
-      <div className="code-nav flex">
-        <nav className='right-nav flex'>
-          <input type="checkbox" id="check" />
-          <label htmlFor="check" className="checkbtn">
-            <i className="fa fa-bars"></i>
-          </label>
-          <label className="logo">
-            <img style={{ width: "50px" }} src="../img/T4.png" alt="box" className="img-fluid" /><NavLink to="/home">Cart.Com</NavLink>
-          </label>
-          <ul className='flex'>
-            <li>
-              <form onSubmit={submit}>
-                <div className='d-flex form-group '>
-                  <input type='text' size={30} id='searchbar' placeholder="Search for products, brands and more" className='form-control form-group-sm' defaultValue={inputValue} onChange={(e) => { setInputValue(e.target.value) }} />
-                  <button type='submit' className='btn btn-primary m-0' style={{ padding: "5px 15px", borderRadius: "3px" }}> <i className="fa fa-search"></i></button>
-                </div>
-              </form>
-            </li>
-            <li><NavLink to="/Products">Products</NavLink></li>
-            <li><NavLink to="/wishlist" className="cart-box">Wishlist <span>{wishlist.length}</span></NavLink></li>
-            <li><NavLink to="/cart" className="cart-box">Cart <span>{cart.length}</span> </NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
-            <li><NavLink to="/myaccount" >User </NavLink></li>
-          </ul>
-        </nav>
-      </div>
-    </>
+  return (<div className="code-nav flex">
+    <nav className='right-nav flex'>
+      <input type="checkbox" id="check" />
+      <label htmlFor="check" className="checkbtn">
+        <i className="fa fa-bars"></i>
+      </label>
+      <label className="logo">
+        <img style={{ width: "50px" }} src="../img/T4.png" alt="box" className="img-fluid" /><NavLink to="/home">Cart.Com</NavLink>
+      </label>
+      <ul className='flex'>
+        <li>
+          <form onSubmit={submit}>
+            <div className='d-flex form-group '>
+              <input type='text' size={30} id='searchbar' placeholder="Search for products, brands and more" className='form-control form-group-sm' defaultValue={inputValue} onChange={(e) => { setInputValue(e.target.value) }} />
+              <button type='submit' className='btn btn-primary m-0' style={{ padding: "5px 15px", borderRadius: "3px" }}> <i className="fa fa-search"></i></button>
+            </div>
+          </form>
+        </li>
+        <li><NavLink to="/Products">Products</NavLink></li>
+        <li><NavLink to="/wishlist" className="cart-box">Wishlist <span>{wishlist.length}</span></NavLink></li>
+        <li><NavLink to="/cart" className="cart-box">Cart <span>{cart.length}</span> </NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+        <li><NavLink to="/myaccount" >User </NavLink></li>
+      </ul>
+    </nav>
+  </div>
   )
 }
 
-export default Navbar
+export default Navbar;
--- frontend/src/page/Slides.jsx
+++ frontend/src/page/Slides.jsx
@@ -5,7 +5,7 @@ function Slides() {
     return (
         <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
@@ -31,11 +31,11 @@ function Slides() {
                 </div> */}
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-                {/* <span class="carousel-control-prev-icon" aria-hidden="true"></span> */}
+                {/* <span className="carousel-control-prev-icon" aria-hidden="true"></span> */}
                 <span className="visually-hidden">Previous</span>
             </button>
             <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-                {/* <span class="carousel-control-next-icon" aria-hidden="true"></span> */}
+                {/* <span className="carousel-control-next-icon" aria-hidden="true"></span> */}
                 <span className="visually-hidden">Next</span>
             </button>
         </div>
