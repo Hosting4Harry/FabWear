9366086771914df7fefad103e28a60b305339ba8
HaraprasadTripathy <haraprasad@evincedev.com>
2023-03-21 11:17:05 +0530

    created login and register controller inside account folder

---
+++ backend/controllers/account/register.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const db = require('../../models');
+
+router.post("/", (req, res) => {
+    const { email, username, password } = req.body;
+    bcrypt.hash(password, saltRounds, async (errr, hash) => {
+        const data = {
+            username,
+            email,
+            password: hash,
+        };
+        if (errr) {
+            console.log(errr);
+        }
+        else {
+            let result;
+            await db.users.findAll({
+                where: {
+                    email: email
+                }
+            }).then(response => {
+                result = response;
+            }).catch(error => {
+                result = error
+            })
+            if (result?.fatal) {
+                console.trace('fatal error: ' + er.message);
+            }
+            else if (result?.length > 0) {
+                res.send({ msg: "User Email Already Present" })
+            }
+            else {
+                await db.users.create(data)
+                    .then(response => {
+                        res.send({ userData: response });
+                    }).catch(error => {
+                        console.log(error);
+                    })
+            }
+        }
+    })
+})
+
+
+
+
+
+
+module.exports = router;
