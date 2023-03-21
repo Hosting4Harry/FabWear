f47d2c2a1c2e176f58307854b341ce0cb3eb0c44
SumitKumarPatro <sumitm@evincedev.com>
2023-03-21 10:49:00 +0530

    changed class to className

--- frontend/src/page/Slides.jsx
+++ frontend/src/page/Slides.jsx
@@ -30,13 +30,13 @@ function Slides() {
                     <img src="../img/E5.webp" className="d-block w-100" style={{ borderRadius: "2em" }} alt="..." />
                 </div> */}
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
                 {/* <span class="carousel-control-prev-icon" aria-hidden="true"></span> */}
-                <span class="visually-hidden">Previous</span>
+                <span className="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
                 {/* <span class="carousel-control-next-icon" aria-hidden="true"></span> */}
-                <span class="visually-hidden">Next</span>
+                <span className="visually-hidden">Next</span>
             </button>
         </div>
     )
