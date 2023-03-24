a325b5bcaf00fe837c74a99d522f8dc8a0bf526e
SumitKumarPatro <sumitm@evincedev.com>
2023-03-24 14:01:16 +0530

    make some changes in css

--- frontend/src/page/Contact.jsx
+++ frontend/src/page/Contact.jsx
@@ -49,7 +49,7 @@ const Contact = () => {
         }).then(response => {
             toast.success(response.data.message || "message send", {
                 position: "bottom-right",
-                autoClose: 5000,
+                autoClose: 1500,
                 hideProgressBar: false,
                 closeOnClick: true,
                 pauseOnHover: true,
@@ -62,14 +62,14 @@ const Contact = () => {
     };
 
     return (
-        <section className="mb-4 container">
+        <section className="mb-4 container form-control" style={{ marginTop: "100px" }}>
             <h2 className="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
-            <p className="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within few
-                matter of hours to help you.</p>
-            <div className="row">
+
+            <div className="row " >
                 <div className="col-md-9 mb-md-0 mb-5">
                     <form id="contact-form" name="contact-form" onSubmit={handleSubmit}>
                         <div className="row">
+                            <p className="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within few minutes to help you.</p>
                             <div className="col-md-6">
                                 <div className="md-form mb-0">
                                     <input type="text" id="name" name="name" className="form-control" required />
