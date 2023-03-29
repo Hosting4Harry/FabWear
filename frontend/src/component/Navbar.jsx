e6c9d160b67ef4467b220fda1475019b1addc9f5
HaraprasadTripathy <haraprasad@evincedev.com>
2023-03-29 15:50:35 +0530

    changed the style display property to none

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -64,7 +64,7 @@ const Navbar = () => {
               <button type='submit' className='btn btn-primary m-0' style={{ padding: "5px 15px", borderRadius: "5px" }}> <i className="fa fa-search"></i></button>
             </div>
             {searchResult.length > 0 &&
-              <div className='' id='searchList' onMouseLeave={hideList} style={{ width: '320px', position: 'absolute', zIndex: 9999, display: 'block' }}>
+              <div className='' id='searchList' onMouseLeave={hideList} style={{ width: '320px', position: 'absolute', zIndex: 9999, display: 'none' }}>
                 <ul className="list-group"  >
                   {searchResult.map((item, i) => {
                     return <li className="list-group-item" key={i}>
