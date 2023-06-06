057cfc31a10d8d95cf6825aaa5b7edcae7ae8e67
Haraprasad Evince <haraprasad@evincedev.com>
2023-06-06 12:43:28 +0530

    minor changes

--- frontend/src/component/AdminSide/Dashboard.jsx
+++ frontend/src/component/AdminSide/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { CChart } from '@coreui/react-chartjs'
 import './Dashboard.css'
+import { Link } from 'react-router-dom'
 import axios from 'axios';
 
 function Dashboard() {
@@ -112,12 +113,13 @@ function Dashboard() {
                         </div>
                     </div>
                     <div className='col-sm-12 col-lg-3 card3 col-md-6 d-flex my-5 py-3 shadow' >
+
                         <div className='pt-3 d-flex' >
                             <div className='pt-3'>
                                 <img src="/img/QR_icon.webp" alt="" style={{ height: "50px" }} />
                             </div>
                             <div style={{ marginLeft: "20px" }}>
-                                <h4 fontWeight="600" fontSize="17px" >Products<br />
+                                <h4 fontWeight="600" fontSize="17px" ><Link to='/admin/products' className='text-dark' style={{ "textDecoration": 'none' }}>Products </Link><br />
                                     {totalProduct.length}
                                 </h4>
                                 <span fontSize="16px" color="grey.600" >In 4 Categories</span>
